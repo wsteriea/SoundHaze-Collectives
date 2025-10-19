095057152a721e0f6b7ccf06682c459b8a747255
wsteriea <micaella.marie02@gmail.com>
2025-10-19 21:34:43 +0800

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/SoundHaze-Collectives/tree/main/soundhaze",
+  base: process.env.VITE_BASE_PATH || "/SoundHaze-Collectives",
 
 })
 
